1333b79b84c22df2b3837d58a7475471b8440c3c
justtryingthingsout <31097061+justtryingthingsout@users.noreply.github.com>
2025-03-08 18:44:23 +0100

    Improve AEA template for decrypted, non-extracted AEAs

--- aea.tcl
+++ aea.tcl
@@ -4,6 +4,7 @@ proc fmtPfid {pfid} {
 	return [switch -- $pfid {
 		0 { format "No encryption, ECDSA Signed (%d)" $pfid }
 		1 { format "Symmetric encryption (%d)" $pfid }
+		105 { format "Decrypted symmetric encryption (%d)" $pfid }
 		2 { format "Symmetric encryption, ECDSA Signed (%d)" $pfid }
 		3 { format "ECDHE Asymmetric Encryption (%d)" $pfid }
 		4 { format "ECDHE Asymmetric Encryption, ECDSA Signed (%d)" $pfid }
@@ -72,35 +73,70 @@ section "Apple Encrypted Archive" {
 	}
 	bytes 32 "Key Derivation Salt"
 	bytes 32 "Root Header HMAC-SHA256"
-	if {$pfid != 0} {
-		bytes 48 "Encrypted Root Header"
-	} else {
+	if {$pfid == 0 || $pfid == 0x69} {
 		section "Root Header" {
 			uint64 -hex "Original File Size"
 			uint64 -hex "Encrypted Archive Size"
-			uint32 -hex "Segment Size"
-			uint32 -hex "Segments per Cluster"
+			set ss [uint32 -hex "Segment Size"]
+			set spc [uint32 -hex "Segments per Cluster"]
 			entry "Compression Algorithm" [fmtCompAlg [ascii 1]] [move -1; format 1]; move 1
 			set ca [uint8]; move -1
 			entry "Checksum Algorithm" [fmtChecksumAlg $ca] 1; move 1
+			switch -- $ca {
+				0 { set cal 0 }
+				1 { set cal 8 }
+				2 { set cal 32 }
+			}
+			set shs [expr {$cal + 8}]
 			bytes 22 "Unknown"
 		}
+	} else {
+		bytes 48 "Encrypted Root Header"
 	}
-	bytes 0x20 "First Cluster Header HMAC-SHA256"
-	if {$pfid == 0} {
-		section "First Cluster" {
-			uint32 -hex "Original Size"
-			uint32 -hex "Compressed Size"
-			switch -- $ca {
-				0 {}
-				1 { uint64 -hex "Murmur Hash" }
-				2 { bytes 0x20 "SHA-256" }
+	bytes 0x20 "Cluster 0 Header HMAC-SHA256"
+	if {$pfid == 0 || $pfid == 0x69} {
+		set j 0
+		while {[pos] < [len]} {
+			section -collapsed [format "Cluster %d" $j] {
+				set clusterEnd 0
+				set dataoff [expr {0x20 * ($spc - 1)}]
+				for {set i 0} {$i < $spc} {incr i} {
+					section -collapsed [format "Segment %d" $i] {
+						section -collapsed [format "Segment Header" $i] {
+							set osz [uint32 -hex "Original Size"]
+							set csz [uint32 -hex "Compressed Size"]
+							switch -- $ca {
+								0 {}
+								1 { uint64 -hex "Murmur Hash" }
+								2 { bytes 0x20 "SHA-256" }
+							}
+						}
+						set curpos [pos]
+						move [expr {($shs * ($spc - 1 - $i)) + (0x20 * ($i + 1))}]
+						bytes 32 "Segment MAC"
+						move [expr {$dataoff - (0x20 * $i)}]
+						if {$csz != 0} {
+							if {$osz > $csz} {
+								bytes $csz "Compressed Segment Data"
+							} else {
+								bytes $csz "Segment Data"
+							}
+						}
+						set clusterEnd [pos]
+						incr dataoff $csz
+						goto $curpos
+						if {$clusterEnd == [len]} {
+							return -level 1
+						}
+					}
+				}
+				bytes 0x20 [format "Cluster %d Header HMAC-SHA256" [expr {$j + 1}]]
+				move [expr {$dataoff + (0x20 * $spc)}]
+				goto $clusterEnd
+				incr j 1
 			}
 		}
-	}
-	if {$pfid == 0} {
-		bytes eof "Data Clusters"
 	} else {
-		bytes eof "Encrypted Data Clusters"
+		bytes eof "Encrypted Clusters"
 	}
 }
