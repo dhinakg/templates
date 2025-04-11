5d6ba25edb88be06c417ba249e485e30d08fe4f6
Dhinak G <17605561+dhinakg@users.noreply.github.com>
2025-04-11 10:28:25 -0400

    PBZ: Make template more robust

--- PBZX.tcl
+++ PBZX.tcl
@@ -1,34 +1,32 @@
 big_endian
 
+include "Utility/General.tcl"
+
+
 requires 0 "70627A"
-ascii 3
-set type [ascii 1]
-
-
-            # "z": zlib.decompressobj,
-            # "x": lzma.LZMADecompressor,
-            # # 4: lz4
-            # # e: lzfse
+set type [ascii 4]
 
 switch -- $type {
-    "z" { entry "Magic" "ZLIB"  4 0}
-    "x" { entry "Magic" "LZMA"  4 0}
-    "4" { entry "Magic" "LZ4"  4 0}
-    "e" { entry "Magic" "LZFSE" 4 0 }
-    default { entry "Magic" "Unknown"  4 0}
+    "pbzz" { entry "Magic" [format "ZLIB (%s)" $type] 4 0}
+    "pbzx" { entry "Magic" [format "LZMA (%s)" $type] 4 0}
+    "pbz4" { entry "Magic" [format "LZ4 (%s)" $type] 4 0}
+    "pbze" { entry "Magic" [format "LZFSE (%s)" $type] 4 0 }
+    default { entry "Magic" "Unknown" 4 0}
 }
 
-uint64 "Block Size"
+set bsize [uint64 "Block Size"]
 
-for {set i 1} {![end]} {incr i} {
-    section "Block $i" {
-        set usize [uint64 "Uncompressed Size"]
-        set size [uint64 "Compressed Size"]
-        if {$usize == $size} {
-            sectionname "Block $i (Uncompressed)"
-            bytes $size "Uncompressed Data"
-        } else {
-            bytes $size "Compressed Data"
+main_guard {
+    for {set i 1} {![end]} {incr i} {
+        section "Block $i" {
+            set usize [uint64 "Uncompressed Size"]
+            set size [uint64 "Compressed Size"]
+            if {$usize == $size} {
+                sectionname "Block $i (Uncompressed)"
+                bytes $size "Uncompressed Data"
+            } else {
+                bytes $size "Compressed Data"
+            }
         }
     }
 }
